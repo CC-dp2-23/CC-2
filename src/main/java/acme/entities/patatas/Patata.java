e3262b0362dfe6e087bb4712e0361e71d1f9f072
Albert0led0 <a.toledo96@gmail.com>
2021-05-30 20:42:14 +0200

    001: patata model completed.

---
+++ src/main/java/acme/entities/patatas/Patata.java
@@ -0,0 +1,43 @@
+package acme.entities.patatas;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import acme.framework.datatypes.Money;
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Patata extends DomainEntity {
+	
+    protected static final long    serialVersionUID    = 1L;
+    
+    @NotBlank
+    @Column(unique = true)
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$")
+    protected String patataTicker;
+    
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+    protected Date patataMoment;
+	
+	@NotNull
+	@Valid
+	protected Money patataValue;
+	
+	@NotNull
+	protected Boolean patataBoolean;
+
+
+}
--- src/main/java/acme/entities/shouts/Shout.java
+++ src/main/java/acme/entities/shouts/Shout.java
@@ -3,6 +3,7 @@ package acme.entities.shouts;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
@@ -12,6 +13,7 @@ import javax.validation.constraints.Past;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.entities.patatas.Patata;
 import acme.framework.entities.DomainEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -47,4 +49,7 @@ public class Shout extends DomainEntity{
 	// -------------- Derived attributes --------------
 	
 	// -------------- Relationships --------------
+	
+	@OneToOne(optional = false)
+	protected Patata patata;
 }
