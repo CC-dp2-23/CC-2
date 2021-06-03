dd98f6ed87ea1a5ed6c1f6d9a9516d2587fdf610
Albert0led0 <a.toledo96@gmail.com>
2021-06-03 14:41:52 +0200

    006: valid patata added

--- src/main/java/acme/entities/shouts/Shout.java
+++ src/main/java/acme/entities/shouts/Shout.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -51,5 +52,6 @@ public class Shout extends DomainEntity{
 	// -------------- Relationships --------------
 	
 	@OneToOne(optional = false)
+	@Valid
 	protected Patata patata;
 }
